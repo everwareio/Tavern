72d1b0b8bfd5326adbbf56f394008a1b526bf00a
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-07-31 17:24:27 +1000

    Fixed config reader to be a little more flexible with whitespace

--- config.rb
+++ config.rb
@@ -1,13 +1,13 @@
 module Config
-    # turn the config file into a series of hashed values, treat $ as comment
-    def Config.parse(source) 
-        settings = Hash.new
-        source.each_line do |line|
-            unless line[0] == '$'
-                values = line.split('=')
-                settings[values[0]] = values[1]
-            end
-        end
-        return settings
+  # turn the config file into a series of hashed values, treat $ as comment
+  def self.parse(source) 
+    settings = Hash.new
+    source.each_line do |line|
+      unless line[0] == '$'
+        values = line.split('=')
+        settings[values[0].strip] = values[1].strip
+      end
     end
+    return settings
+  end
 end
