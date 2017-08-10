b8b03c4b14af8ab650e056d9bb0b55bc238b70c4
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-08-10 18:57:08 +1000

    Fixed error where the config parser would attempt to strip a nil line

--- config.rb
+++ config.rb
@@ -3,11 +3,12 @@ module Config
   def self.parse(source)
     settings = Hash.new
     source.each_line do |line|
-      unless line[0] == '$'
+      if line.length > 1 and line[0] != "$"
         values = line.split('=')
         settings[values[0].strip] = values[1].strip
       end
     end
+    puts settings
     return settings
   end
 end
