dd6319fa24e78941f0cd26f5dc0cfb834cc0cba1
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-08-23 13:30:00 +1000

    Updated to check for required arguments

--- tavern.rb
+++ tavern.rb
@@ -10,26 +10,36 @@ Config.parse()
 
 # run the program based on the ARGV
 if ARGV[0] == "install"
-  if ARGV[2] == "-l"
-    Core.install_local(ARGV[1], Config.get('lib'), Config.get('os'))
-  else
-    Core.install(ARGV[1], Config.get('lib'), Config.get('os'))
+  unless ARGV[1] == ""
+    if ARGV[2] == "-l"
+      Core.install_local(ARGV[1], Config.get('lib'), Config.get('os'))
+    else
+      Core.install(ARGV[1], Config.get('lib'), Config.get('os'))
+    end
   end
 elsif ARGV[0] == "uninstall"
-  if ARGV[2] == "-l"
-    Core.uninstall_local(ARGV[1])
-  else
-    Core.uninstall(ARGV[1])
+  unless ARGV[1] == ""
+    if ARGV[2] == "-l"
+      Core.uninstall_local(ARGV[1])
+    else
+      Core.uninstall(ARGV[1])
+    end
   end
 elsif ARGV[0] == "update"
-  Core.update(ARGV[1], Config.get('lib'), Config.get('os'))
+  unless ARGV[1] == ""
+    Core.update(ARGV[1], Config.get('lib'), Config.get('os'))
+  end
 elsif ARGV[0] == "info"
   Core.info()
 elsif ARGV[0] == "config"
   if ARGV[1] == "set"
-    Config.set(ARGV[2], ARGV[3])
+    unless ARGV[3] == ""
+      Config.set(ARGV[2], ARGV[3])
+    end
   elsif ARGV[1] == "get"
-    puts Config.get(ARGV[2])
+    unless ARGV[2] ==  ""
+      puts Config.get(ARGV[2])
+    end
   end
 end
 
