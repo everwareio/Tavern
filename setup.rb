d1f52f0d27d8566b77fbb10ad2aa42c7435782a3
“belgradebotnet” <26587568+belgradebotnet@users.noreply.github.com>
2017-08-25 13:50:39 +1000

    Fixed setup script

--- setup.rb
+++ setup.rb
@@ -4,9 +4,3 @@ require 'fileutils'
 unless File.directory?("Storeroom")
     FileUtils.mkdir_p("Storeroom")
 end
-
-# creates a index file to store packages
-unless File.exists?("Storeroom/.packageindex")
-    f = File.open("Storeroom/.packageindex", "w+")
-    f.close
-end
